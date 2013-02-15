3caf081e51cb7a1f25f674410d4a6b61c5a9269c
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-15 20:54:59 +0100

    Renamed package name

---
+++ BuddyOlympics/src/com/awezumTree/buddyolympics/cache/FileHelper.java
@@ -0,0 +1,12 @@
+package com.awezumTree.buddyolympics.cache;
+
+public class FileHelper {
+	
+	protected void writeToFile(String value) {
+		
+	}
+	
+	protected String readFromFile() {
+		return "";
+	}
+}
