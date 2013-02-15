cdbb57ab4674a9006db9c0472efe01ca6b086441
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-16 00:45:58 +0100

    changed error handling to Log.e

--- BuddyOlympics/src/com/awezumTree/buddyolympics/cache/CacheFileObject.java
+++ BuddyOlympics/src/com/awezumTree/buddyolympics/cache/CacheFileObject.java
@@ -3,6 +3,7 @@ package com.awezumTree.buddyolympics.cache;
 import org.json.JSONObject;
 
 import android.content.Context;
+import android.util.Log;
 
 public class CacheFileObject {
 	
@@ -12,22 +13,22 @@ public class CacheFileObject {
 		fh = new FileHelper(cachename);
 	}
 
-	public JSONObject getCachedData(Context context) throws CacheException {
+	public JSONObject getCachedData(Context context) {
 		JSONObject cached = new JSONObject();
 		try {
 			String raw = fh.readFromFile(context);
 			cached = new JSONObject(raw);
 		} catch (Exception e) {
-			throw new CacheException(e);
+			Log.e("CacheError", "getCachedData " + e.getMessage());
 		}
 		return cached;
 	}
 	
-	public void setCacheData(JSONObject json, Context context) throws CacheException {
+	public void setCacheData(JSONObject json, Context context) {
 		try {
 			fh.writeToFile(json.toString(), context);
 		} catch (Exception e) {
-			throw new CacheException(e);
+			Log.e("CacheError", "setCacheData " + e.getMessage());
 		}
 	}
 }
