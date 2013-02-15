eae76908bb00c27b3c31ee19e55d8d20aa24ef10
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-15 20:52:29 +0100

    init new package JSONFiles and FileHelper class

---
+++ BuddyOlympics/src/com/awezumTree/buddyolympics/JSONFiles/FileHelper.java
@@ -0,0 +1,12 @@
+package com.awezumTree.buddyolympics.JSONFiles;
+
+public class FileHelper {
+	
+	public void writeToFile(String value) {
+		
+	}
+	
+	public String readFromFile() {
+		return "";
+	}
+}
