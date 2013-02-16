996ff5dca1d35c3cb653584b9e30f472d84e52cd
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-16 15:02:40 +0100

    Shows stupid list in addfriend. Should show friends to add

--- BuddyOlympics/src/com/awezumTree/buddyolympics/activities/AddFriendActivity.java
+++ BuddyOlympics/src/com/awezumTree/buddyolympics/activities/AddFriendActivity.java
@@ -1,19 +1,44 @@
 package com.awezumTree.buddyolympics.activities;
 
-import com.awezumTree.buddyolympics.R;
-import com.awezumTree.buddyolympics.domain.Runner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.awezumTree.buddyolympics.R;
 
 public class AddFriendActivity extends Activity {
+	
+	List<Map<String, String>> listmapshits = new ArrayList<Map<String, String>>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_firend);
+		
+		initList();
+		
+		ListView lv = (ListView) findViewById(R.id.friendListView);
+		
+		SimpleAdapter simpleAdpt = new SimpleAdapter(this, listmapshits, android.R.layout.simple_list_item_1, new String[] {"friend"}, new int[] {android.R.id.text1});
 
+		lv.setAdapter(simpleAdpt);
+	}
+	
+	private void initList() {
+		listmapshits.add(createFriendListEntry("Scarlett"));
+		listmapshits.add(createFriendListEntry("Bar Rafaeli"));
+	}
+	
+	private HashMap<String, String> createFriendListEntry(String name) {
+	    HashMap<String, String> friend = new HashMap<String, String>();
+	    friend.put("friend", name);
+	     
+	    return friend;
 	}
 }
